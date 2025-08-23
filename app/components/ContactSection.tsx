e54aac95201b93282e699c76e9b54cc88995c73d
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2025-08-24 00:59:11 +0530

    Update contacts

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -30,7 +30,7 @@ export default function ContactSection() {
 									/>
 									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 								</svg>
-								<span>San Francisco, CA</span>
+								<span>Kolkata, India</span>
 							</div>
 						</motion.div>
 						<div className="flex flex-col items-center gap-6 mb-8">
