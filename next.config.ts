94c8cc7b9033b84f0754dfa8d4823d517a1a6f5c
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2025-08-25 01:27:12 +0530

    [gh-pages] Trial

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/yajnab.github.io/' : '',
+  basePath: isProd ? '/yajnab.github.io' : '',
+  output: 'export'
 };
 
 export default nextConfig;
